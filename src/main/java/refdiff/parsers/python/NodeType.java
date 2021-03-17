fc7edccf1186c15f37503358d5a33631db438339
QassemNa <alnasserqassem@gmail.com>
2021-03-17 19:06:37 +0300

    updated parser.py

---
+++ src/main/java/refdiff/parsers/python/NodeType.java
@@ -0,0 +1,7 @@
+package refdiff.parsers.python;
+
+public class NodeType {
+	public static final String FUNCTION = "Function";
+	public static final String FILE = "File";
+	public static final String CLASS = "Class";
+}
--- test-data/parser/python/example.py
+++ test-data/parser/python/example.py
@@ -1,27 +1,8 @@
-def sum(a, b):
-    c = a + b
-    print(multi(b, c))
-    return c
+class test:
+    def sum(a,b):
+        c = a + b
+        return c
 
-
-def multi(a, b):
-    c = a * b
-    return c
-
-
-class calculation():
-    class basic():
-        def sum(self, a, b):
-            c = a + b
-            print('addition is ', c)
-            [print(self.substraction(b, a-1)) for a in range(0,5)]
-
-            return c
-
-        def substraction(self, a, b):
-            c = a - b
-            return c
-
-
-temp = calculation.basic()
-temp.sum(5, 6)
+    def multi(q,w):
+        e = q*w
+        return e
