4a71de875ccaa456eb3f37492546006b62ebd91b
QassemNa <alnasserqassem@gmail.com>
2021-03-17 18:38:31 +0300

    removed namespace from function and classes to None

--- src/main/java/refdiff/parsers/python/NodeType.java
+++
@@ -1,7 +0,0 @@
-package refdiff.parsers.python;
-
-public class NodeType {
-	public static final String FUNCTION = "Function";
-	public static final String FILE = "File";
-	public static final String CLASS = "Class";
-}
--- test-data/parser/python/example.py
+++ test-data/parser/python/example.py
@@ -1,8 +1,27 @@
-class test:
-    def sum(a,b):
-        c = a + b
-        return c
+def sum(a, b):
+    c = a + b
+    print(multi(b, c))
+    return c
 
-    def multi(q,w):
-        e = q*w
-        return e
+
+def multi(a, b):
+    c = a * b
+    return c
+
+
+class calculation():
+    class basic():
+        def sum(self, a, b):
+            c = a + b
+            print('addition is ', c)
+            [print(self.substraction(b, a-1)) for a in range(0,5)]
+
+            return c
+
+        def substraction(self, a, b):
+            c = a - b
+            return c
+
+
+temp = calculation.basic()
+temp.sum(5, 6)
